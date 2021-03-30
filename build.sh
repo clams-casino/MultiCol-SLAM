c4e6d94f8503177a65481412c49959249f07c414
mike <mike.zhang.619@gmail.com>
2021-03-30 02:09:06 +0200

    updated build.sh script

--- build.sh
+++ build.sh
@@ -1,10 +1,36 @@
+echo "Configuring and building Thirdparty/OpenCV ..."
+
+cd ThirdParty/OpenCV
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+-DBUILD_opencv_apps=OFF \
+-DBUILD_opencv_dnn=OFF \
+-DBUILD_opencv_flann=OFF \
+-DBUILD_opencv_java_bindings_gen=OFF \
+-DBUILD_opencv_js_bindings_gener=OFF \
+-DBUILD_opencv_ml=OFF \
+-DBUILD_opencv_objdetect=OFF \
+-DBUILD_opencv_photo=OFF \
+-DBUILD_opencv_python3=OFF \
+-DBUILD_opencv_python_bindings_g=OFF \
+-DBUILD_opencv_python_tests=OFF \
+-DBUILD_opencv_shape=OFF \
+-DBUILD_opencv_superres=OFF \
+-DBUILD_opencv_ts=OFF \
+-DBUILD_opencv_video=OFF \
+-DBUILD_opencv_videoio=OFF \
+-DBUILD_opencv_videostab=OFF
+make -j4
+
+cd ../../
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
-cd ThirdParty/DBoW2
+cd DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 
 cd ../../
@@ -17,7 +43,8 @@ cd g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make
+make -j4
+
 
 echo "Configuring and building Thirdparty/opengv ..."
 
@@ -26,7 +53,8 @@ cd ../../OpenGV
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
+
 
 echo "Configuring and building MultiCol-SLAM ..."
 cd ../../../
@@ -34,4 +62,4 @@ echo $(pwd)
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
